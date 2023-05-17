677c66294ae02cf132b2b27e499c084d14d26f31
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2023-05-17 12:22:12 +0530

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./styles.css";
 import DemoPage from "./demo";
+import LandingPage from "./components/landing";
 export default function App() {
   return (
     <div className="App">
-      <DemoPage />
+      {/* <DemoPage /> */}
+      <LandingPage />
     </div>
   );
 }
